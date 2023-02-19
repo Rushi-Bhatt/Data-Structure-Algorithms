249a68ded17a66c898a2370d52c8f408e721b89f
Rushi Bhatt <rushi@Rushis-MacBook-Air.local>
2023-02-18 23:42:52 -0800

    Initial commit

---
+++ DSA/DataStructures.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+struct Stack<Element: Equatable> : Equatable {
+  private var storage: [Element] = []
+  
+  init() { }
+  
+  init(withArray elements: [Element]) {
+    storage = elements
+  }
+  
+  mutating func push(_ element: Element) {
+    storage.append(element)
+  }
+  
+  @discardableResult
+  mutating func pop() -> Element? {
+    return storage.popLast()
+  }
+  
+  func peek() -> Element? {
+    return storage.last
+  }
+  
+  func isEmpty() -> Bool {
+    return storage.isEmpty // OR peek() == nil
+  }
+  
+}
+
+extension Stack: CustomStringConvertible {
+  var description: String {
+    storage.map { (each) -> String in
+      return "\(each)"
+    }.joined(separator: " ")
+  }
+}
+
+extension Stack: ExpressibleByArrayLiteral {
+  typealias ArrayLiteralElement = Element
+  init(arrayLiteral elements: Self.ArrayLiteralElement...) {
+    self.storage = elements
+  }
+}
